f74a12fe856f7e3b212ac3f10d4cfe6f602e7877
DocKnowledge <chris.jones424@outlook.com>
2023-03-28 15:44:55 +0100

    Added Order Script

---
+++ EmpowerAgain_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerAgain_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerAgain_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("90642C5F-B0E3-417A-A363-19C478F4DE6C")]
+[assembly: AssemblyVersion("1.0.0.0")]
